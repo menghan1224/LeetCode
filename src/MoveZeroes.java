d0805971374458a46c7689a7456f734ea7ee064d
menghan <13501175561@163.com>
2017-06-01 22:21:43 +0800

    2016-0601-2202

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -3,6 +3,13 @@
  */
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
-
+        int pos=0;
+        for(int num:nums){
+            if(num!=0){
+                nums[pos++]=num;
+            }
+        }
+        for(int i=pos;i<nums.length;i++)
+            nums[pos]=0;
     }
 }
---
+++ src/TwoSumII.java
@@ -0,0 +1,22 @@
+/**
+ * Created by 28479 on 2017/6/1.
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        int i=0;
+        int j=numbers.length-1;
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                res[0]=i+1;
+                res[1]=j+1;
+                return res;
+            }
+            else if(sum>target){
+                j--;
+            }else i++;
+        }
+        return res;
+    }
+}
