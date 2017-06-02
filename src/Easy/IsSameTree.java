2db803d9d8d68e02d548103607b6c1a477950cb9
menghan <13501175561@163.com>
2017-06-02 11:01:31 +0800

    2017-0602-1101

---
+++ src/Easy/IsSameTree.java
@@ -0,0 +1,17 @@
+package Easy;
+
+import DataStructure.TreeNode;
+
+/**
+ * Created by menghan on 2017/6/2.
+ */
+public class IsSameTree {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) return true;
+        if(p!=null||q==null) return false;
+        if(p.val==q.val) return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        return  false;
+
+    }
+
+}
