7d5da0d1510789fe9daed71b27c1dc1129d62931
menghan <13501175561@163.com>
2017-06-04 13:09:08 +0800

    2017-0604-1308

---
+++ src/Easy/MergeTwoSortedLists.java
@@ -0,0 +1,23 @@
+package Easy;
+
+import DataStructure.ListNode;
+
+/**
+ * Created by menghan on 2017/6/2.
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode tmp;
+        if(l1.val<l2.val){
+            tmp=l1;
+            tmp.next=mergeTwoLists(l1.next,l2);
+        }
+        else{
+            tmp=l2;
+            tmp.next=mergeTwoLists(l1,l2.next);
+        }
+        return tmp;
+    }
+}
